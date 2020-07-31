3efae731f1006680a6b54c7566ac109566e3f5cd
Daniel Kyman <danny.kyman@gmail.com>
2020-07-31 10:22:47 -0700

    Updates

---
+++ precache-manifest.10bf9e3887d278c09232ad28455c0743.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc88219d3569bbc4524637eff6b8fca",
+    "url": "/conways-game-of-life/index.html"
+  },
+  {
+    "revision": "33be54e674a9787c41bf",
+    "url": "/conways-game-of-life/static/css/main.6bd60265.chunk.css"
+  },
+  {
+    "revision": "33b5c382b4079618ea39",
+    "url": "/conways-game-of-life/static/js/2.503678c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conways-game-of-life/static/js/2.503678c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33be54e674a9787c41bf",
+    "url": "/conways-game-of-life/static/js/main.00334282.chunk.js"
+  },
+  {
+    "revision": "de07b28fa652a1a2b888",
+    "url": "/conways-game-of-life/static/js/runtime-main.c46c1675.js"
+  },
+  {
+    "revision": "00c8e4b0297bdb103d2e3ead977dd5bf",
+    "url": "/conways-game-of-life/static/media/BLADRMF_.00c8e4b0.TTF"
+  },
+  {
+    "revision": "81a8e5876b8e6c8568e63e083476bf41",
+    "url": "/conways-game-of-life/static/media/grid-background.81a8e587.jpg"
+  },
+  {
+    "revision": "a46c29bc13206fdb2ebf5db4a16ec5cc",
+    "url": "/conways-game-of-life/static/media/welcome-background.a46c29bc.jpg"
+  }
+]);
